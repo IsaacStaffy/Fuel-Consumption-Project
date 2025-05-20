4702f21dcac5438b82f390fdd116bb675d40e380
ratatatmatt8 <masbury09@icloud.com>
2025-05-20 16:58:17 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,45 @@ import os
 import math
 import gdown
 import googlemaps
+import openrouteservice
+import requests
+import json
+from geopy.distance import geodesic
 
-# ------------------------- Utility Functions -------------------------
+API_KEY = "AIzaSyAynX3T_Zw6hbCBsLdFng9sdDwZHEdAg5I"
+ORS_client = openrouteservice.Client(key='5b3ce3597851110001cf62482e7b727c0455400493ff86701141d359')
+ORS_API_KEY = "5b3ce3597851110001cf62482e7b727c0455400493ff86701141d359"
 
+#API_KEY = st.secrets["API_KEY"]
+gmaps = googlemaps.Client(key=API_KEY)
+
+
+# ------------------------- Functions -------------------------
+
+# -- Utility
+# Truncate float f to n decimal places without rounding
+def truncate(f, n):
+    if n == 0:
+        return int(f)
+    return math.floor(f * 10 ** n) / 10 ** n
+
+# Converts meters_to_feet
+def meters_to_feet(meters):
+  feet = meters * 3.28084
+  return feet
+
+# Convert meters to miles
+def meters_to_miles(meters):
+    return meters / 1609.344
+
+
+# Convert grams per kilometer to tonnes per mile
+def gpkm_to_tpm(gpkm):
+    grams_per_tonne = 1_000_000
+    kilometers_per_mile = 1.60934
+    return (gpkm / grams_per_tonne) * kilometers_per_mile
+
+# --- Google maps
 # Address to Coordinates (Geocoding)
 def get_lat_lng(gmaps, address):
     try:
@@ -25,18 +61,78 @@ def get_lat_lng(gmaps, address):
         print(f"An error occurred during geocoding: {e}")
         return None
 
-# Truncate float f to n decimal places without rounding
-def truncate(f, n):
-    if n == 0:
-        return int(f)
-    return math.floor(f * 10 ** n) / 10 ** n
+# Gets elevation of a coordinate in meters
+def get_elevation(gmaps, lat, lng):
+    try:
+        elevation_result = gmaps.elevation((lat, lng))
+        if elevation_result:
+            elevation = elevation_result[0]['elevation']
+            return elevation
+        else:
+            print("Elevation lookup failed.")
+            return None
+    except Exception as e:
+        print("An error occurred during elevation lookup: {e}")
+        return None
 
-# Convert grams per kilometer to tonnes per mile
-def gpkm_to_tpm(gpkm):
-    grams_per_tonne = 1_000_000
-    kilometers_per_mile = 1.60934
-    return (gpkm / grams_per_tonne) * kilometers_per_mile
+# -- Open route service maps api (after ors_client has been defined)
+# Snaps coordinates to nearest to road
+def snap_to_road(lat, lon):
+    result = ors_client.pelias_reverse((lon, lat))
+    snapped = result['features'][0]['geometry']['coordinates']  # [lon, lat]
+    return [snapped[1], snapped[0]]  # Return as [lat, lon]
 
+# Locate gas stations: returns [[[lon1, lat1], [lon2, lat2]], [{'name': 'Chevron'}, {'name': 'Shell'}]] up to 3 stations
+def locate_gas(lat, lon, search_radius=2000, amount=3, ORS_API_KEY=ORS_API_KEY):
+  body = {"request":"pois","geometry":{"geojson":{"type":"Point","coordinates":[lon, lat]},"buffer":search_radius},"filters":{"category_ids":[596]},"limit":amount}
+  headers = {
+      'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+      'Authorization': ORS_API_KEY,
+      'Content-Type': 'application/json; charset=utf-8'
+      }
+
+  call = requests.post('https://api.openrouteservice.org/pois', json=body, headers=headers)
+  response = json.loads(call.text)
+
+  try:
+    if response["features"]:
+      return [[feature['geometry']['coordinates'] for feature in response["features"]],
+              [feature['properties'].get('osm_tags', {}) for feature in response["features"]]]
+    else:
+      print("no features found")
+  except:
+    print(f"issue finding features: {response}")
+
+# subdevides a route returns pandas df of points,
+def subdivide_route(df, input, units="miles"):
+  if units == "meters":
+    interval = input
+  elif units == "miles":
+    interval = input * 1609.34
+
+  # Initialize cumulative distance
+  cumulative_distance = [0]
+  for i in range(1, len(df)):
+    dist = geodesic(
+      (df.iloc[i-1]['lat'], df.iloc[i-1]['lon']),
+      (df.iloc[i]['lat'], df.iloc[i]['lon'])
+    ).meters
+    cumulative_distance.append(cumulative_distance[-1] + dist)
+
+  df['cumulative_dist'] = cumulative_distance
+
+  # Get points at every interval meters
+  output_points = []
+  next_dist = 0
+
+  for i in range(1, len(df)):
+    if df.iloc[i]['cumulative_dist'] >= next_dist:
+      output_points.append(df.iloc[i])
+      next_dist += interval
+
+  return pd.DataFrame(output_points).drop(columns=['cumulative_dist'])
+
+# -- Model functions
 # Label encode a column using a specified class order
 def LabelEncode(input_df, column, order):
     le = preprocessing.LabelEncoder()
@@ -53,6 +149,7 @@ def load_model():
     with open(model_path, "rb") as f:
         return pickle.load(f)
 
+# -- Main body dunctions
 # UI for collecting user inputs
 def get_input_data():
     st.title("Fuel Consumption and Road trip calculator")
@@ -169,10 +266,70 @@ if st.session_state.ran:
     preds = [truncate(x, decimal_places) for x in st.session_state.prediction[0]]
     st.table(pd.DataFrame([preds], columns=titles))
 
-    mileage = st.number_input("Trip Mileage Estimate", value=None)
     gas_price = st.number_input("Estimated Gas Price", value=None)
     tank_size = st.number_input("Gas Tank Size (gallons)", value=None)
     highway_ratio = st.slider("Highway Driving %", 0, 100, value=75) / 100
+
+    mileage = None
+
+    # Use google maps api: disply features
+    if bypass == "Use":
+      start = st.text_input("Start point")
+      end = st.text_input("End point")
+      subdivision = st.slider("tenths of a mile between route markers", 1, 1000)
+      subdivision /= 10
+
+      if start and end:
+        start_coords = get_lat_lng(gmaps, start)
+        end_coords = get_lat_lng(gmaps, end)
+
+        if start_coords and end_coords:
+          start_lat, start_lon = start_coords
+          end_lat, end_lon = end_coords
+
+          # Get route
+          route = ORS_client.directions(
+              coordinates=[[start_lon, start_lat], [end_lon, end_lat]],
+              profile='driving-car',
+              format='geojson'
+          )
+          
+
+          # Decode the polyline from geometry (or use GeoJSON coordinates directly)
+          geometry = route['features'][0]['geometry']['coordinates']
+
+          # Convert geometry to a DataFrame
+          route_points = pd.DataFrame(geometry, columns=['lon', 'lat'])
+
+          route_display_points = subdivide_route(route_points, subdivision)
+          route_display_points['color'] = '#FF0000'
+          route_display_points['size'] = 0.1
+
+          end_points = pd.DataFrame({"lat": [start_lat, end_lat], "lon": [start_lon, end_lon]})
+          end_points['color'] = '#0000FF80'
+          end_points['size'] = 75
+
+          final_points = pd.concat([route_display_points, end_points], ignore_index=True)
+
+          st.map(final_points, color='color', size='size')
+
+          start_elevation = get_elevation(gmaps, start_lat, start_lon)
+          end_elevation = get_elevation(gmaps, end_lat, end_lon)
+          avg_elevation = (meters_to_feet(start_elevation)  + meters_to_feet(end_elevation)) / 2
+
+          if avg_elevation >= 1750:
+            st.write(f":warning: *Warning: The elevation you are driving at is {int(avg_elevation)} and at this elevation engine power and effciency will be reduced by about %{int((avg_elevation / 1000) * 3)}* :warning:")
+          if (end_elevation - start_elevation) >= 500:
+            st.write(f":warning: *Warning: Net elevation change ascends {int(meters_to_feet(end_elevation - start_elevation))} feet which may reduce fuel efficiency* :warning:")
+          if (end_elevation - start_elevation) <= -500:
+            st.write(f"*Net elevation change descends {int(meters_to_feet(end_elevation - start_elevation))} feet which may increase fuel efficiency*")
+
+          st.link_button("Go to Google maps route", f"https://www.google.com/maps/dir/?api=1&origin={start_lat},{start_lon}&destination={end_lat},{end_lon}&travelmode=driving")
+          
+          mileage = meters_to_miles(route['features'][0]['properties']['summary']['distance'])
+          st.write(f"Mileage: {truncate(mileage, 1)}")
+    else:
+      mileage = st.number_input("Trip Mileage Estimate", value=None)
 
     if mileage:
         h_miles = highway_ratio * mileage
@@ -187,32 +344,11 @@ if st.session_state.ran:
     if mileage and gas_price and tank_size:
         st.table(pd.DataFrame([[total_cost, tank_refills]], columns=["Trip Cost", "Tank Refills"]))
 
-    # Use google maps api: disply features
-    if bypass == "Use":
-      start = st.text_input("Start point")
-      end = st.text_input("End point")
-
-      API_KEY = st.secrets["API_KEY"]
-      gmaps = googlemaps.Client(key=API_KEY)
-
-      if start and end:
-        start_coords = get_lat_lng(gmaps, start)
-        end_coords = get_lat_lng(gmaps, end)
-
-        if start_coords and end_coords:
-          start_lat, start_lon = start_coords
-          end_lat, end_lon = end_coords
-
-          points = pd.DataFrame({"lat": [start_lat, end_lat], "lon": [start_lon, end_lon]})
-          st.map(points)
-
-          st.link_button("Go to Google mpas route", f"https://www.google.com/maps/dir/?api=1&origin={start_lat},{start_lon}&destination={end_lat},{end_lon}&travelmode=driving")
-
     if mileage:
       emissions_tpm = gpkm_to_tpm(st.session_state.prediction[0][2])
       st.subheader(f"Total Emissions (Tonnes): {truncate(emissions_tpm * mileage, 10)}")
 
-      st.write(f"Donate ${truncate((emissions_tpm * mileage) * 18.68, 2)}, to offset carbon emmisions") 
+      st.write(f"Donate ${truncate((emissions_tpm * mileage) * 18.68, 2)}, to offset carbon emmisions")
 
       st.link_button("Donate Here", "https://www.cooleffect.org/store/donate")
 
@@ -220,5 +356,3 @@ if st.session_state.ran:
       st.write("Cooleffect.org takes carbon offset donations and uses them to fund emmision reducing projects around the world!")
       st.write("Learn more here:")
       st.page_link("https://www.cooleffect.org/about-us", label="CoolEffect.org", icon="🌎")
-
-
